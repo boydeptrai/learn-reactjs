9a417f67aa20578b102e2e73f4d255bf9ae819ca
Tuan Tran <langthamht047@gmail.com>
2022-04-29 07:31:09 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
+import React from 'react';
 // import './App.css';
+import {Link, Route,NavLink } from 'react-router-dom';
 import TodoFeature from './features/Todo/pages';
 import AlbumFeature from './features/Album/pages';
+
+
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
-      {/* <AlbumFeature /> */}
+     Header
+      <p><Link to="/todos">Todos</Link></p>
+      <p><Link to="/albums">Albums</Link></p>
+
+      <p><NavLink to="/todos">Todos</NavLink></p>
+      <p><NavLink to="/albums">Albums</NavLink></p>
+      
+      <Route path="/todos" component ={TodoFeature}/>
+      <Route path="/albums" component ={AlbumFeature}/>
+      
+     Footer
     </div>
   );
 }
